63c2c09e8de1d00ab127b43182f3803ec15eac35
Fernando Ramirez <framirez1997@gmail.com>
2019-02-20 10:39:02 -0600

    Add new NgModule to hold material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    AppMaterialModule, // HAS TO BE AFTER BrowserModule
     AppRoutingModule
   ],
   providers: [],
