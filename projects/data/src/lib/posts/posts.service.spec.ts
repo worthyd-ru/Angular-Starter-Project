17e8a886f68720c6adafec301bed9b08ff193051
Daniel Worthy <daniel@revunit.com>
2020-11-12 08:46:54 -0600

    Fixing unit tests

--- projects/data/src/lib/posts/posts.service.spec.ts
+++ projects/data/src/lib/posts/posts.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PostsService } from './posts.service';
+import { PostsModule } from './posts.module';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PostsService', () => {
   let service: PostsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [PostsModule, HttpClientModule]
+    });
     service = TestBed.inject(PostsService);
   });
 
