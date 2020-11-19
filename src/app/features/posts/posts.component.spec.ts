1956d2e1acfb7aa80fcfd43936d2217fb8647d60
Daniel Worthy <daniel@revunit.com>
2020-11-19 13:54:24 -0600

    adding fixed unit tests

--- src/app/features/posts/posts.component.spec.ts
+++ src/app/features/posts/posts.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
 import { PostsComponent } from './posts.component';
 import { PostsModule } from './posts.module';
 
@@ -10,7 +11,19 @@ describe('PostsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, PostsModule]
+      imports: [HttpClientModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [PostsComponent],
+      providers: [
+        provideMockStore({
+          initialState: {
+            posts: {
+              entities: {},
+              ids: []
+            }
+          }
+        })
+      ]
     }).compileComponents();
   });
 
